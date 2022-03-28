dfdf74bbae38dcd8024f7fc7f20866edc6b46755
ankit <ankitvashisthngh@gmail.com>
2022-03-28 11:04:23 +0530

    body color change

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function drawBall()
 {
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-    ctx.fillStyle = ball.visible ? '#0095dd' : '#0095dd'; // change here
+    ctx.fillStyle = ball.visible ? '#0095dd' : 'transparent'; // change here
     ctx.fill();
     ctx.closePath();
 }
